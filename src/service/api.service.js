4ce9aa0b64ab17a682e8a796eef57b9427ee0b73
Bharath-2656 <bharath2000madhu@gmail.com>
2025-03-18 12:11:39 +0530

    Refactor addReview function and implement getReviews API

--- src/service/api.service.js
+++ src/service/api.service.js
@@ -50,15 +50,25 @@ export const deleteMovie = async (movieId) => {
   }
 };
 
-export const addReview = async (movieId, reviewData) => {
+export const addReview = async (reviewData) => {
   try {
     const response = await axiosInstance.post(
-      `/reviews/${movieId}`,
+      `/reviews/movie`,
       reviewData
     );
     return response.data;
   } catch (error) {
     console.error("Error adding review:", error);
+    throw error;
+  }
+};
+
+export const getReviews = async (movieId) => {
+  try {
+    const response = await axiosInstance.get(`/reviews/movie/${movieId}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching reviews:", error);
     throw error;
   }
 };
